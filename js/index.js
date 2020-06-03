992cd99db0869a1de7ecdba24426a26d6917f3f6
Leon Brown <leonbrown@MacBook-Pro.local>
2020-06-03 16:21:20 +0100

    add about section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-
   // Fade in title
   $(".hero-title").delay(200).animate({ opacity: 1, top: 0 }, 800);
+
+  // Fade in navbar
+  $(".navbar").delay(800).animate({ opacity: 1 }, 700);
 });
