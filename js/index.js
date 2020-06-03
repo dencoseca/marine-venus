402407b7c45d85fa7f9de3d8812228be285f9042
Leon Brown <leonbrown@MacBook-Pro.local>
2020-06-03 21:38:10 +0100

    add specs button contextual text

--- js/index.js
+++ js/index.js
@@ -4,4 +4,27 @@ $(document).ready(function () {
 
   // Fade in navbar
   $(".navbar").delay(800).animate({ opacity: 1 }, 700);
+
+  // Color the navbar on scroll
+  $(document).scroll(function () {
+    const navbar = $(".navbar");
+    const viewHeight = $(window).height();
+    const scrollTop = $(window).scrollTop();
+    if (scrollTop > viewHeight - 20) {
+      navbar.addClass("colored");
+    } else {
+      navbar.removeClass("colored");
+    }
+  });
+
+  // Specs button contextual text change
+  const specsBtn = $(".specs-btn");
+  specsBtn.on("click", function () {
+    const isExpanded = specsBtn.attr("aria-expanded");
+    if (isExpanded === "false") {
+      specsBtn.text("Hide full specs");
+    } else {
+      specsBtn.text("Show full specs");
+    }
+  });
 });
